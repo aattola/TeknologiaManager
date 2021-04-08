703e4f9fdbdee45ec914a423bf93642cfa4d2ebb
JEFFe <andrejeff.leevi@gmail.com>
2021-04-08 14:37:23 +0300

    Remove workflow new icon appbar fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
+const customTitlebar = require('custom-electron-titlebar');
+
+new customTitlebar.Titlebar({
+  backgroundColor: customTitlebar.Color.fromHex('#444'),
+});
+
 render(<App />, document.getElementById('root'));
